cabf7d0d72fea199d8a10ddb3f4485b13f6d641f
supriya-mallick <supriyamallick132@gmail.com>
2022-01-24 21:50:25 -0800

    First Commit

---
+++ selenium4/src/test/java/example/web1Test.java
@@ -0,0 +1,15 @@
+package example;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class web1Test {
+  @Test
+  public void f() {
+	  System.setProperty("webdriver.chrome.driver","C:\\Users\\supriya\\Downloads\\chromedriver_win32 (1)\\chromedriver.exe");
+	  WebDriver d1;
+	  d1=new ChromeDriver();
+	  d1.get("https://phptravels.com/demo/");
+  }
+}
